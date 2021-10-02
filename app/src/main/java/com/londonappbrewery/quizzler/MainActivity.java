cdcf94a1f81a85146364e02b7ae303644c17b948
supunperera <7hashd@gmail.com>
2021-10-02 07:19:10 +0530

    Initial commit

--- app/src/main/java/com/londonappbrewery/quizzler/MainActivity.java
+++ app/src/main/java/com/londonappbrewery/quizzler/MainActivity.java
@@ -75,7 +75,7 @@ public class MainActivity extends Activity {
             @Override
             public void onClick(View view) {
                 checkAnswer(true);
-                updateQuestion();
+                updateQuestion();kasck
             }
         });
         mFalseButton.setOnClickListener(new View.OnClickListener() {
